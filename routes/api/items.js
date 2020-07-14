78c0efe050f86942c0db313d59ea16958d8b2d3d
cipdv <cip.devries@gmail.com>
2020-07-14 13:22:53 -0400

    end of video 3 - all is working well, not sure about db

--- routes/api/items.js
+++ routes/api/items.js
@@ -14,14 +14,25 @@ router.get('/', (req, res)=>{
 });
 
 //2.13 @route POST api/items
-//@desc get all items
+//@desc post an item
 //@access public
 router.post('/', (req, res)=>{
     const newItem = new Item({
         name: req.body.name
     });
-    newItem.save().then(item=> res.json(item));
+    newItem.save()
+    .then(item=> res.json(item));
 });
+
+//2.14 @route DELETE api/items/:id
+//@desc delete an item
+//@access public
+router.delete('/:id', (req, res)=>{
+    Item.findById(req.params.id)
+        .then(item => item.remove().then(()=> res.json({success: true})))
+        .catch(err=>res.status(404).json({success: false}));
+});
+    
 
 module.exports = router;
 
