f541a3ed5e0ec3312b70ac7eb08f25b486677353
cipdv <cip.devries@gmail.com>
2020-07-20 19:51:15 -0400

    up to video 5, all is working

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import AppNavBar from './components/AppNavBar'
+import AppNavBar from './components/AppNavBar';
+import ShoppingList from './components/ShoppingList';
+import { Provider} from 'react-redux';
+import store from './store';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <AppNavBar />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <AppNavBar />
+        <ShoppingList />
+      </div>
+    </Provider>
   );
 }
 
